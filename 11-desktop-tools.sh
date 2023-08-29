9661857cecf0d59acf0569b684d21778ba450a0c
Anthony Oteri <anthony.oteri@edgeware.tv>
2023-08-29 15:29:41 -0400

    Add script to install desktop utilities

---
+++ 11-desktop-tools.sh
@@ -0,0 +1,74 @@
+#!/bin/bash                   
+#
+# -----------------------------------------------------------------------------
+# Installer Script for desktop tools
+# -----------------------------------------------------------------------------
+#
+
+echo
+echo ' ____  _____ ____  _  _______ ___  ____  '
+echo '|  _ \| ____/ ___|| |/ /_   _/ _ \|  _ \ '
+echo '| | | |  _| \___ \| ` /  | || | | | |_) |'
+echo '| |_| | |___ ___) | . \  | || |_| |  __/ '
+echo '|____/|_____|____/|_|\_\ |_| \___/|_|    '
+echo 
+
+while true; do
+    read -p "Do you want to start the installation now (Y/n): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* )
+            exit;
+        break;;
+        * )  echo "Please answer yes or no.";;
+    esac
+done
+
+# -----------------------------------------------------------------------------
+# Install dependent packages 
+# -----------------------------------------------------------------------------
+
+yay --noconfirm -S \
+    alacritty \
+    bibata-cursor-theme \
+    brave-bin \
+    breeze \
+    breeze-gtk \
+    chromium \
+    dunst \
+    exa \
+    figlet \
+    freerdp \
+    lxappearance \
+    mousepad \
+    mpv \
+    neovim \
+    nitrogen \
+    pavucontrol \
+    pfetch \
+    picom \
+    python-click \
+    python-pip \
+    python-psutil \
+    python-rich \
+    rofi \
+    rofi-calc \
+    scrot \
+    slock \
+    starship \
+    thunar \
+    ttf-fira-code \
+    ttf-firacode-nerd \
+    ttf-fira-sans \
+    ttf-font-awesome \
+    tumbler \
+    vlc \
+    xdg-desktop-portal-gtk \
+    xfce4-power-manager
+
+echo "Done!"
+
+                                         
+
