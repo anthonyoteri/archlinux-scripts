fdb86d74be8d14d8af0e866bb0b723994d71a593
Anthony Oteri <anthony.oteri@edgeware.tv>
2023-08-29 16:13:23 -0400

    Add script for ZSH

---
+++ 09-zsh.sh
@@ -0,0 +1,38 @@
+#!/bin/bash                   
+#
+# -----------------------------------------------------------------------------
+# Installer Script for neofetch
+# -----------------------------------------------------------------------------
+#
+
+echo
+echo ' _________  _   _ '
+echo '|__  / ___|| | | |'
+echo '  / /\___ \| |_| |'
+echo ' / /_ ___) |  _  |'
+echo '/____|____/|_| |_|'
+echo 
+
+while true; do
+    read -p "Do you want to start the installation now (Y/n): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* )
+            exit;
+        break;;
+        * )  echo "Please answer yes or no.";;
+    esac
+done
+
+# -----------------------------------------------------------------------------
+# Install exa
+# -----------------------------------------------------------------------------
+
+yay --noconfirm -S \
+    oh-my-zsh \
+    zsh 
+
+echo "Done!"
+
